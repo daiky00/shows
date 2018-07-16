50454a05043fa2014d3e788d5c9eb92b63bfa146
Francisco Pena <project.01@hotmail.com>
2018-07-15 23:34:03 -0500

    made header component

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+<header class="header">
+    <span class="header-spacer"></span>
+    <slot></slot>
+</header>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class Header extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.header {
+    background: linear-gradient(to right, #f5ea3c, #008f68);
+    top: 0;
+    left: 0;
+    right: 0;
+    padding: 16px;
+    display: flex;
+    height: 18px;
+}
+
+.header-spacer {
+    flex: 1 1 auto;
+}
+</style>
