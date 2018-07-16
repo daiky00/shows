2563c6b065c6486fcc1256f3af927a5e87479994
Francisco Pena <project.01@hotmail.com>
2018-07-15 23:36:06 -0500

    modify title component to look good

---
+++ src/components/Title.vue
@@ -0,0 +1,22 @@
+<template>
+<h1 class="title">{{title}}</h1>
+</template>
+
+<script lang="ts">
+import {
+    Component,
+    Prop,
+    Vue
+} from 'vue-property-decorator';
+
+@Component
+export default class Title extends Vue {
+    @Prop() private title!: string;
+}
+</script>
+<style lang="scss" scoped>
+.title {
+    border-bottom: 6px dotted #018f68;
+    padding-bottom: 20px;
+    }
+</style>
