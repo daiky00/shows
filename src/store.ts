a9df1a2ec1cfc851ea7faca7a9c0301a4801e620
Francisco Pena <project.01@hotmail.com>
2018-07-15 23:49:52 -0500

    made store typescript friendtly

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,47 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { MutationTree, ActionTree, GetterTree } from 'vuex'
+import axios from 'axios';
+ 
+Vue.use(Vuex);
 
-Vue.use(Vuex)
+const API = 'https://gist.githubusercontent.com/memikequinn/475999c535ef4e1341f1262484244c68/raw/d77d0f6f4d7e53d7bf5dd7bd1997b27d291d4b0b/db.json';
+
+interface State {
+  shows: Array<any>
+}
+
+const state: State  = {
+  shows: []
+}
+
+const actions: ActionTree<State, any> = {
+  LoadShows({ commit }): any {
+    axios({url: API}).then((response) => {
+      const shows = JSON.parse(JSON.stringify(response.data));
+      commit('SetShows', shows);
+    }, (error) => {
+      console.log(error);
+    });
+  }
+}
+
+const mutations: MutationTree<State> = {
+  SetShows(state, shows ) {
+    state.shows = shows;
+  }
+}
+
+const getters: GetterTree<State, any> = {
+    GetShows(state) { 
+      return state.shows
+    }
+}
+
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+  state,
+  actions,
+  mutations,
+  getters
 })
