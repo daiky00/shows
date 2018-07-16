ed42244680e3634db2f762fd9305e299cc3f2f5c
Francisco Pena <project.01@hotmail.com>
2018-07-15 23:39:42 -0500

    created navigation component

---
+++ src/components/Navigation.vue
@@ -0,0 +1,46 @@
+<template>
+<ul class="navigation">
+    <li class="navigation-item">
+        <router-link class="navigation-link" to="/">Shows</router-link>
+    </li>
+    <li class="navigation-item">
+        <router-link class="navigation-link" to="/about">About</router-link>
+    </li>
+</ul>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class Navigation extends Vue {
+    constructor() {
+        super();
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.navigation {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    align-items: center;
+}
+
+.navigation-item {
+    display: inline-block;
+    margin-right: 16px;
+}
+
+.navigation-link {
+    color: honeydew;
+    text-decoration: none;
+
+    &:hover {
+        border-bottom: 2px solid honeydew;
+    }
+}
+</style>
