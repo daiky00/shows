d47c65f1abb4c1352cd16e2423895d498d31962e
Francisco Pena <project.01@hotmail.com>
2018-07-15 23:48:32 -0500

    added new layout and remove old one

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,49 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<div id="app">
+    <Header>
+        <Navigation/>
+    </Header>
+    <container>
+        <router-view/>
+    </container>
+</div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script lang="ts">
+import {
+    Component,
+    Vue
+} from 'vue-property-decorator';
+import Header from '@/components/Header.vue' // @ is an alias to /src
+import Navigation from '@/components/Navigation.vue'; // @ is an alias to /src
+import VueFractionGrid from "vue-fraction-grid";
+
+Vue.use(VueFractionGrid, {
+    container: '1200px',
+    gutter: '24px',
+    approach: 'mobile-first',
+    breakpoints: {
+        compact: '0px 996px'
     }
-  }
+});
+
+@Component({
+    components: {
+        Header,
+        Navigation
+    },
+})
+export default class Home extends Vue {}
+</script>
+
+<style lang="scss">
+body {
+    margin: 0;
+}
+
+#app {
+    font-family: Montserrat, HelveticaNeue-Light, helvetica neue light, helvetica neue, Helvetica, Arial, lucida grande, sans-serif;
+    background: linear-gradient(to top, transparent, #fdf9ea);
+    min-height: 100vh;
 }
 </style>
